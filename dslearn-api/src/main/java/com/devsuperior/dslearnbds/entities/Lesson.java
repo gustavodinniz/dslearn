04567727e6f5377b124711ff0476ba5d4176ea39
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-12 20:13:24 -0300

    bug fix

--- dslearn-api/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ dslearn-api/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -2,9 +2,7 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_lesson")
@@ -24,6 +22,9 @@ public abstract class Lesson implements Serializable {
     @ManyToOne
     @JoinColumn(name = "section_id")
     private Section section;
+
+    @OneToMany(mappedBy = "lesson")
+    private List<Deliver> deliveries = new ArrayList<>();
 
     @ManyToMany
     @JoinTable(name = "tb_lessons_done",
@@ -80,6 +81,10 @@ public abstract class Lesson implements Serializable {
 
     public Set<Enrollment> getEnrollmentsDone() {
         return enrollmentsDone;
+    }
+
+    public List<Deliver> getDeliveries() {
+        return deliveries;
     }
 
     @Override
