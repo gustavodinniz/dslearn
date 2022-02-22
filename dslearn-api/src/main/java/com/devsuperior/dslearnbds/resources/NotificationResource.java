142724b6da648d9d7715f0ad6cee6c30b487303e
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-02-22 20:57:02 -0300

    query to return not or all notifications

--- dslearn-api/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
+++ dslearn-api/src/main/java/com/devsuperior/dslearnbds/resources/NotificationResource.java
@@ -7,8 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +19,10 @@ public class NotificationResource {
     private NotificationService service;
 
     @GetMapping
-    public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(Pageable pageable) {
-        Page<NotificationDTO> page = service.notificationsForCurrentUser(pageable);
+    public ResponseEntity<Page<NotificationDTO>> notificationsForCurrentUser(
+            @RequestParam(name = "unreadOnly", defaultValue = "false") Boolean unreadOnly,
+            Pageable pageable) {
+        Page<NotificationDTO> page = service.notificationsForCurrentUser(unreadOnly, pageable);
         return ResponseEntity.ok().body(page);
     }
 }
